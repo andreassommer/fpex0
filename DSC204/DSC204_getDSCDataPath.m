d9127e7036617ce5232df64dfce5419bea06d6bb
Andreas Sommer <andreas.sommer@iwr.uni-heidelberg.de>
2022-08-25 15:30:46 +0200

    Deleted superfluous files

--- DSC204/DSC204_getDSCDataPath.m
+++
@@ -1,9 +0,0 @@
-function datpath = DSC204_getDSCDataPath()
-  % path = DSC204_getDSCDataPath()
-  %
-  % Returns the path to the DSC measurement files (*.csv)
-  %
-  
-  datpath = '/home/asommer/Documents/modELTES/DSC204_F1_Phoenix_Messungen/alles/';
-  
-end
