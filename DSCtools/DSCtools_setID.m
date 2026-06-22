bd43c3ecdb908ec91ab02cb45fc2af964471cf19
Andreas Sommer <code@andreas-sommer.eu>
2026-06-22 18:55:58 +0200

    Reorganized data fields in DSCdata structure

--- DSCtools/DSCtools_setID.m
+++ DSCtools/DSCtools_setID.m
@@ -97,9 +97,9 @@ function id = make_id_from_description(DSCsample)
    count = length(DSCsample);
    id = cell(count, 1);
    for k = 1:count
-      samp = DSCsample(k).rawData.desc.SAMPLE;
-      mass = DSCsample(k).rawData.desc.SAMPLEMASSmg;
-      rate = DSCsample(k).rawData.Tinfo.Tstep;
+      samp = DSCsample(k).desc.SAMPLE;
+      mass = DSCsample(k).desc.SAMPLEMASSmg;
+      rate = DSCsample(k).Tinfo.Tstep;
       id{k} = sprintf('%s__%smg__rate%+5.2f', samp, mass, rate);
    end
    % if there are multiple with the same ID now, add unique number to each
