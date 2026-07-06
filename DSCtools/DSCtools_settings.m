2f111def6efd0a4a847fd24bc8e1e5bbbf5da9b3
Andreas Sommer <code@andreas-sommer.eu>
2026-07-06 13:55:35 +0200

    Simplified DSCtools_settings() -- can now used named preset

--- DSCtools/DSCtools_settings.m
+++ DSCtools/DSCtools_settings.m
@@ -1,17 +1,13 @@
-function settings = DSCtools_settings(varargin)
-% settings = DSCtools_settings(varargin)
+function varargout = DSCtools_settings(preset)
+% settings = DSCtools_settings(preset)
 %
-% Generates, updates, or replaces DSCtools settings.
+% Chooses a settings preset.
 %
-%  INPUT:   key-value-pairs:
-%         'replace' --> replaces global DSCTOOLS_GLOBAL_SETTINGS with new settings
-%          'update' --> updates global DSCTOOLS_GLOBAL_SETTINGS with new settings
+% INPUT:     preset --> named setting, e.g. 'default'. See code for details.
 %
-%  OUTPUT:  settings -> settings structure
+% OUTPUT:  settings --> settings structure
 %
-% Special call:  DSCtools_settings('ACTIVATE') loads and activates default settings
-%
-% Side effects: May update or replace global DSCTOOLS_GLOBAL_SETTINGS.
+% GLOBAL SIDE EFFECT: Updates or replaces global DSCTOOLS_GLOBAL_SETTINGS
 %
 % Author:  Andreas Sommer, Aug2022, Jun2026
 % andreas.sommer@iwr.uni-heidelberg.de
@@ -19,26 +15,84 @@ function settings = DSCtools_settings(varargin)
 %
 
 
-% get the encoding of the machine
-enc = DSCtools_getEncodingDefaults();
+% access to global settings structure
+global DSCTOOLS_GLOBAL_SETTINGS         %#ok<GVMIS>
+
+
+% choose setting
+if (nargin == 0), error('No setting preset specified.'); end
+
+% process command
+switch upper(preset)
+   case {'DEFAULT', 'DEFAULTS', 'ACTIVATE'}    % 'ACTIVATE' for backward compatibility
+      settings = get_default_settings();
+
+   case 'NETZSCH2026'
+      settings = get_default_settings();
+      settings.blds_preset = 'netzsch2026';
+      % settings.addCP_scaleToRate = true;
+      settings.addCP_scaleByMass = false;
+
+   case 'RELOAD'
+      preset = getSetting(DSCTOOLS_GLOBAL_SETTINGS, 'preset');
+      DSCtools_settings(preset);
+      return
+
+   otherwise
+      error('Unknown settings: %s', command)
+end
+
+% add name of preset
+settings.preset = preset;
+
+% set the global variable and return variable
+DSCTOOLS_GLOBAL_SETTINGS = settings;
+if (nargout > 0)
+   varargout{1} = settings;
+end
+
+% finito
+makeMessage('Activated global settings preset "%s"\n', preset);
+return
+
+end % of function
+
+%% HELPERS
+
+
+
+
+
+
+
+
+function defaults = get_default_settings()
 
 % DEFAULT SETTINGS  ---  see the respective files for explanations
 % ================
 
 % addCP
-defaults.addCP_debugMode   = true;
-defaults.addCP_scaleByMass = true;
-defaults.addCP_scaleToRate = true;
-defaults.addCP_TrangeWarn  = false;
+defaults.addCP_debugMode    = true;
+defaults.addCP_scaleByMass  = true;
+defaults.addCP_scaleToRate  = true;
+defaults.addCP_TrangeWarn   = false;
 defaults.addCP_defaultSignalSource = 'uV';
-defaults.addCP_defaultTmax = 160;
-defaults.addCP_defaultTmin =  55;
+defaults.addCP_defaultTmax  = 200;
+defaults.addCP_defaultTmin  =  20;
 
 % warnings if header columns (of data section) cannot be retrieved
 defaults.dataMAT_warn_if_column_not_found  = true;
 defaults.dataMAT_warn_if_column_not_unique = true;
 
+% baseline settings 
+defaults.getBaselinePrimitive_resolution = 1000; % resolution for baseline function (number of points)
+defaults.getBaseline_haltOnWarning       = true; % halt if getBaseline has warnings
+
+% baseline detection settings: blds -- choose preset
+defaults.blds_preset = 'default';
+
 % expected units
+enc = DSCtools_getEncodingDefaults();  % get the encoding of the machine
 defaults.expectedUnits = struct( ...
            'Time'        , 'min' , ...
            'Temperature' , sprintf('%cC', enc.deg), ...    % degree Celsius
@@ -47,73 +101,7 @@ defaults.expectedUnits = struct( ...
            'Scaling'     , 'uV/mW' ...
            );
 
+% for backup of settings
+defaults.previousSettings = [];
 
-% =================
-% Do not edit below
-
-
-% access to global settings structure
-global DSCTOOLS_GLOBAL_SETTINGS         %#ok<GVMIS>
-
-% default values for input arguments
-replaceCurrentSettings = false;
-updateCurrentSettings = false;
-
-% shortcut for "nice" call using single "ACTIVATE" keyword
-if (nargin == 1) && strcmpi(varargin{1},'ACTIVATE')
-   settings = DSCtools_settings('replace',true);
-   return
 end
-
-% process arguments
-argList  = varargin;
-argCount = length(argList);
-if mod(argCount, 2) == 1; error('Invalid number of arguments'); end
-% REPLACEMENT?
-if olHasOption(argList, 'replace')
-   replaceCurrentSettings = olGetOption(argList, 'replace'); 
-   argList  = olRemoveOption(argList, 'replace');
-   argCount = argCount - 2;
-end
-% UPDATES?
-if olHasOption(varargin, 'update')
-   updateCurrentSettings = olGetOption(argList, 'update');
-   argList  = olRemoveOption(argList, 'update'); 
-   argCount = argCount - 2;
-end
-
-
-
-% if settings are to be updated, use current settings instead of defaults
-settings = defaults;
-if updateCurrentSettings
-   settings = DSCTOOLS_GLOBAL_SETTINGS;
-end
-
-
-% PROCESS ARGS
-for k = 1:2:argCount
-   argName = argList{k};    % get setting name
-   argVal  = argList{k+1};  % always exists, as argCount is even (see check above)
-   try % try to store the setting with specified name
-      settings.(argName) = argVal;
-   catch
-      if     isnumeric(argName), error('Invalid setting at position #%g detected: "%g"', k, argName);
-      elseif isstring(argName) , error('Invalid setting at position #%g detected: "%s"', k, argName);
-      elseif ischar(argName)   , error('Invalid setting at position #%g detected: "%s"', k, argName);
-      else                     , error('Invalid setting at position #%g detected: class %s', k, class(argName));
-      end
-   end
-end
-
-
-
-% check if settings should replaced or updated
-if replaceCurrentSettings || updateCurrentSettings
-   DSCTOOLS_GLOBAL_SETTINGS.previousSettings = [];        % remove old settings backup
-   settings.previousSettings = DSCTOOLS_GLOBAL_SETTINGS;  % backup old settings
-   DSCTOOLS_GLOBAL_SETTINGS = settings;                   % activate settings
-end
-
-
-
